b3fc90cb630ed2f8557c0e575e2e0f4bd71ca3f1
Antoine Fauville <antoine.fauville@fluffyfairygames.com>
2018-06-27 10:11:30 +0200

    tile system WorldManager + GameManager

---
+++ CodeSession_1/Assets/Scripts/WorldManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class WorldManager : MonoBehaviour {
+
+    [SerializeField] private int _rows;
+    [SerializeField] private int _colomns;
+
+    [SerializeField] private Vector2 _initialPosition;
+    [SerializeField] private Vector2 _offSetTile;
+
+    [SerializeField] private GameObject _PrefabTile;
+    [SerializeField] private GameObject _TileParent;
+
+    public void BuildWorld()
+    {
+        Vector2 _positionTile = _initialPosition;
+
+        for (int y = 0; y < _colomns; y++)
+        {
+            for (int x = 0; x < _rows; x++)
+            {
+                InstantiateTile(_positionTile,x,y);
+                _positionTile.x += _offSetTile.x;
+            }
+            _positionTile.x = _initialPosition.x;
+            _positionTile.y += _offSetTile.y;
+        }
+	}
+
+    private void InstantiateTile(Vector2 instantiatedTilePosition, int row, int colomn)
+    {
+        GameObject _tile;
+        _tile = Instantiate(_PrefabTile, instantiatedTilePosition, Quaternion.Euler(0, 0, 0));
+        _tile.transform.SetParent(_TileParent.transform);
+        _tile.name = "_Tile_" + row + "_" + colomn;
+    }
+}
